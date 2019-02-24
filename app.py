55205925284126a829d0293d25bd361352a3643f
code-i-o <code-i-o@outlook.com>
2019-02-24 12:23:00 +0530

    Initial commit

---
+++ app.py
@@ -0,0 +1,66 @@
+print("Nitish")
+print('o----')
+print(' ||||')
+print('*' * 10)
+price = 10
+price = 20
+rating = 4.9
+print(price)
+print(rating)
+is_published = False
+print(is_published)
+
+print('*' * 15)
+patient_Name = "John Smith"
+patient_Age = '20'
+patient_Status = "New"
+print(patient_Name)
+print(patient_Age)
+print(patient_Status)
+
+print('*' * 15)
+
+# name = input('What is your name?')
+# color = input('What is your Favourite color?')
+# print(name+' likes '+color)
+# birth_year = input('Birth Year: ')
+# age = 2019 - int(birth_year)
+# print(age)
+
+# pound = input("What is your weight in pound? ")
+# kg = float(pound) * 0.45
+# print("Your weight in KG is " + str(kg) + "kg")
+# print(kg)
+
+# STRINGS
+
+course = 'Python for Beginners'
+course1 = "Python's for Beginners"
+course2 = 'Python for "Beginners"'
+email = ''' 
+Hi John,
+
+Thank you for your response!!
+
+'''
+print(course)
+print(course1)
+print(course2)
+print(email)
+print(course[0])
+print(course[-1])
+print(course[0:3])
+print(course[1:])
+print(course[:5])
+print(course[1:-1])
+
+# Formatted Strings
+first = 'John'
+last = 'Smith'
+message = first + ' [' + last + '] ' + 'is a coder'
+msg = f'{first} [{last}] is a coder'
+print(message)
+print(msg)
+
+# String Methods
+print(len(course))
